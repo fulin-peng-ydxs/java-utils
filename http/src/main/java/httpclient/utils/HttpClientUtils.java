55435b80d1e67ef464c5594d7e7874910abef0f8
pengfulin <1162855925@qq.com>
2024-09-12 15:00:38 +0800

    -优化文件流处理响应方式 -优化多部门请求处理

--- http/src/main/java/httpclient/utils/HttpClientUtils.java
+++ http/src/main/java/httpclient/utils/HttpClientUtils.java
@@ -1,6 +1,9 @@
 package httpclient.utils;
 
 import json.jackson.utils.JsonUtils;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
@@ -26,6 +29,7 @@ import org.apache.http.util.EntityUtils;
 import javax.net.ssl.SSLContext;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -56,7 +60,7 @@ public abstract class HttpClientUtils {
      */
     public static  <T> T execute(RequestType requestType, String url, Map<String,Object> params,Map<String,String> headers, Class<T> targetType,
                                  String targetName, String statusName, String statusValue, String errorName) throws Exception {
-        try (CloseableHttpClient httpClient = createHttpClient(false)) {
+        try (CloseableHttpClient httpClient = createHttpClient(true)) {
             return execute(httpClient,requestType,url,params,headers,targetType,targetName,statusName,statusValue,errorName);
         }
     }
@@ -96,11 +100,11 @@ public abstract class HttpClientUtils {
             httpRequest= new HttpGet(url);
         }else{ //POST&其他请求
             String contentType =null;
-            boolean isNotJson=headers!=null && (contentType = headers.get("Content-Type"))!=null && !contentType.equals(MimeType.APPLICATION_JSON);
+            boolean isNotJson=headers!=null && (contentType = headers.get("Content-Type"))!=null && !contentType.contains(MimeType.APPLICATION_JSON);
             HttpPost httpPost= new HttpPost(url);
             httpRequest= httpPost;
             if(isNotJson) {
-                if (contentType.equals(MimeType.URL_ENCODED_FORM)) {  //url编码表单处理
+                if (contentType.contains(MimeType.URL_ENCODED_FORM)) {  //url编码表单处理
                     // 添加请求参数
                     List<NameValuePair> urlParameters = new ArrayList<>();
                     params.forEach((key,value)->{
@@ -157,7 +161,7 @@ public abstract class HttpClientUtils {
      */
     public static <T> T executeUrlEncodedForm(String url, Map<String,Object> params,Map<String,String> headers,Class<T> targetType,
                                               String targetName,String statusName, String statusValue, String errorName) throws Exception {
-        try(CloseableHttpClient httpClient = createHttpClient(false)){
+        try(CloseableHttpClient httpClient = createHttpClient(true)){
             return executeUrlEncodedForm(httpClient,url, params,headers,targetType,targetName, statusName, statusValue, errorName);
         }
     }
@@ -218,7 +222,7 @@ public abstract class HttpClientUtils {
     public static <T> T executeMutilPart(String url,Map<String,Object> params,Map<String,String> headers,Class<T> targetType,
                                          String targetName,String statusName, String statusValue, String errorName,
                                          InputStream fileInputStream,String fileName) throws Exception {
-        try (CloseableHttpClient httpClient = createHttpClient(false)) {
+        try (CloseableHttpClient httpClient = createHttpClient(true)) {
             HttpPost httpPost = new HttpPost(url);
             //设置请求头
             if(headers!=null){
@@ -228,15 +232,17 @@ public abstract class HttpClientUtils {
             }
             //设置请求体：多部分数据构建
             MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
-            //添加参数：文件，并设置数据类型：二进制类型
-            /*application/octet-stream 是一种 MIME 类型（Multipurpose Internet Mail Extensions），它通常用于表示二进制数据文件的内容类型。
-            这个 MIME 类型没有特定的数据格式或结构，它通常用于指示数据是未知的、不可解释的二进制数据。
-            application/octet-stream 可以用于描述各种类型的文件，包括但不限于：未知文件类型的二进制数据。二进制文件，如图像、音频、视频文件。 压缩文件，如 ZIP、GZIP、TAR 文件。 可执行文件，如可执行程序或脚本文件。
-            当您收到一个 HTTP 响应，其内容类型被标记为 application/octet-stream 时，这意味着服务器正在传输二进制数据，但它不提供有关数据内容的详细信息。
-            通常，这种情况下，您需要根据您的应用程序的需要来处理这些数据，例如，将它们保存到文件或执行其他操作。
-            application/octet-stream 的主要作用是通知接收端，它不应该尝试解释数据内容，而应该将数据保存为原始的二进制形式。这对于传输各种文件和数据类型非常有用，因为它确保数据的完整性和保密性。*/
-            entityBuilder.addBinaryBody("file",fileInputStream,
-                    ContentType.APPLICATION_OCTET_STREAM,fileName);
+            if(fileInputStream!=null){
+                //添加参数：文件，并设置数据类型：二进制类型
+                /*application/octet-stream 是一种 MIME 类型（Multipurpose Internet Mail Extensions），它通常用于表示二进制数据文件的内容类型。
+                这个 MIME 类型没有特定的数据格式或结构，它通常用于指示数据是未知的、不可解释的二进制数据。
+                application/octet-stream 可以用于描述各种类型的文件，包括但不限于：未知文件类型的二进制数据。二进制文件，如图像、音频、视频文件。 压缩文件，如 ZIP、GZIP、TAR 文件。 可执行文件，如可执行程序或脚本文件。
+                当您收到一个 HTTP 响应，其内容类型被标记为 application/octet-stream 时，这意味着服务器正在传输二进制数据，但它不提供有关数据内容的详细信息。
+                通常，这种情况下，您需要根据您的应用程序的需要来处理这些数据，例如，将它们保存到文件或执行其他操作。
+                application/octet-stream 的主要作用是通知接收端，它不应该尝试解释数据内容，而应该将数据保存为原始的二进制形式。这对于传输各种文件和数据类型非常有用，因为它确保数据的完整性和保密性。*/
+                entityBuilder.addBinaryBody("file",fileInputStream,
+                        ContentType.APPLICATION_OCTET_STREAM,fileName);
+            }
             //添加其他参数
             if(params!=null){
                 params.forEach((key,value)->{
@@ -248,10 +254,15 @@ public abstract class HttpClientUtils {
             //设置请求体字符集
             entityBuilder.setCharset(StandardCharsets.UTF_8);
             httpPost.setEntity(entityBuilder.build());
-            log.debug("HttpClient文件上传调用服务：{}",url);
+            log.debug("HttpClient多部分请求调用服务：{}",url);
             //执行请求
             return abstractResponse(httpClient.execute(httpPost), targetType, targetName, statusName, statusValue, errorName);
         }
+    }
+
+    public static <T> T executeMutilPart(String url,Map<String,Object> params,Map<String,String> headers,Class<T> targetType,
+                                         String targetName,String statusName, String statusValue, String errorName) throws Exception {
+        return executeMutilPart(url,params,headers,targetType,targetName,statusName,statusValue,errorName,null,null);
     }
 
 
@@ -266,18 +277,33 @@ public abstract class HttpClientUtils {
         Header contentType = response.getFirstHeader("Content-Type");
         HttpEntity entity = response.getEntity();
         //TODO 后续补充文件涉及的其他信息处理和类型判断完善，例如文件名、响应类型等
-        if(statusCode ==200 && targetType== ByteArrayOutputStream.class && MimeType.APPLICATION_OCTET_STREAM.equals(contentType.getValue())){ //文件流
+        if(statusCode ==200 && contentType.getValue().contains(MimeType.APPLICATION_OCTET_STREAM)){ //文件流
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
             byte[] buffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = entity.getContent().read(buffer)) != -1) {
                 byteArrayOutputStream.write(buffer, 0, bytesRead);
             }
-            return (T) byteArrayOutputStream;
+            if(targetType== ByteArrayOutputStream.class )
+                return (T) byteArrayOutputStream;
+            else{
+                FileResponse fileResponse = new FileResponse();
+                String contentDisposition = response.getFirstHeader("Content-Disposition").getValue();
+                if(contentDisposition!=null){
+                    String[] fileNames = contentDisposition.split(";");
+                    String fileName = fileNames[fileNames.length - 1].trim().replace("\"","");
+                    fileName=fileName.toLowerCase().startsWith("filename=") ? fileName.substring(9):null;
+                    if(fileName!=null){
+                        fileResponse.setFileName(URLDecoder.decode(fileName, "UTF-8"));
+                    }
+                }
+                fileResponse.setContent(byteArrayOutputStream);
+                return (T)fileResponse;
+            }
         }
         String responseJson = EntityUtils.toString(entity, "UTF-8");
         log.debug("HttpClient调用结果：{}",responseJson);
-        if(statusCode==500)
+        if(statusCode!=200)
             throw new RuntimeException("远程服务调用异常："+responseJson);
         //如果targetName&statusName均为空，则直接将响应结果转换成targetType类型直接返回
         if(targetName==null&&statusName==null)
@@ -372,6 +398,19 @@ public abstract class HttpClientUtils {
         public static final String URL_ENCODED_FORM = "application/x-www-form-urlencoded";
         public static final String APPLICATION_JSON = "application/json";
         public static final String APPLICATION_OCTET_STREAM = "application/octet-stream";
+    }
+
+    /**
+     * 文件响应
+     * 2024/9/12 下午2:03
+     * @author fulin-peng
+     */
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FileResponse{
+        private String fileName;
+        private ByteArrayOutputStream content;
     }
 
 }
